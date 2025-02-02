9ad73ebf6fdb3dab8510d0011416e57c3697ec0a
Sanket Raut <sanket@digitalbit.in>
2025-02-02 06:07:43 +0000

    ARCH - New feature to create entities through CLI

---
+++ src/Console/AddEntityCommand.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace PostboxCMS\Desk\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
+
+use PostboxCMS\Desk\Console\Models\Entities;
+use Schema;
+
+class AddEntityCommand extends Command
+{
+
+    use Concerns\InteractsWithDockerComposeServices;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cms:add-entity';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new CMS entity';
+
+    /**
+     * Process entity for database entry
+     */
+    protected function processEntry($entry)
+    {
+        $entry['slug'] = isset($entry['slug']) && $entry['slug'] !== null ? $entry['slug'] : strtolower($entry['name']);
+        $entry['icon'] = isset($entry['icon']) && $entry['icon'] !== null ? $entry['icon'] : 'fa-square';
+        $entry['model'] = isset($entry['model']) && $entry['model'] !== null ? $entry['model'] : ucfirst($entry['name']);
+
+        return $entry;
+    }
+
+    /**
+     * Create table using Schema object
+     */
+    public function generateSchema($model)
+    {
+        try {
+            // Generate table
+            Schema::connection('mysql')->create(strtolower($model), function ($schema) {
+                $schema->increments('id');
+                $schema->timestamps();
+            });
+            // Generate model
+            $stub = file_get_contents(__DIR__ . "/../../stubs/entity-model.stub");
+            $entityModel = str_replace(["{{model}}", "{{table}}"], [$model, strtolower($model)], $stub);
+
+            file_put_contents($this->laravel->basePath('app/Models/' . $model . '.php'), $entityModel);
+            return true;
+        } catch (\Exception $e) {
+            return $this->error('DB Error: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $data = [
+            'name' => $this->textFieldPrompt('Enter the name of the entity','', true),
+            'description' => $this->textFieldPrompt('Enter the description of the entity','', true),
+            'model' => $this->textFieldPrompt('Enter model for your entity', '', true),
+            'slug' => $this->textFieldPrompt('Enter slug for your entity'),
+            'icon' => $this->textFieldPrompt('Enter fontawesome icon for your entity'),
+        ];
+
+        $validator = Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:1000',
+        ]);
+
+        if ($validator->fails()) {
+            $this->error('Validation failed: ' . implode(', ', $validator->errors()->all()));
+            return 1;
+        }
+
+        try {
+            $entity = $this->processEntry($data);
+            Entities::create($entity);
+            $this->generateSchema($entity['model']);
+            $this->info('Entity created successfully.');
+        } catch (\Exception $exception) {
+            $this->error('DB operation failed: ' . $exception->getMessage());
+            return 1;
+        }
+
+        return 0;
+    }
+}
--- src/DeskServiceProvider.php
+++ src/DeskServiceProvider.php
@@ -5,6 +5,7 @@ namespace PostboxCMS\Desk;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 use PostboxCMS\Desk\Console\AddCommand;
+use PostboxCMS\Desk\Console\AddEntityCommand;
 use PostboxCMS\Desk\Console\AddUserCommand;
 use PostboxCMS\Desk\Console\InstallCommand;
 use PostboxCMS\Desk\Console\PublishCommand;
@@ -37,6 +38,7 @@ class DeskServiceProvider extends ServiceProvider implements DeferrableProvider
                 PublishCommand::class,
                 AddUserCommand::class,
                 SetupCommand::class,
+                AddEntityCommand::class,
             ]);
         }
     }
